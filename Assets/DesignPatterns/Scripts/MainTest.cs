2073288a9cc9fd708c3c8d026be562278cd57b8f
HarryWang <646334288@qq.com>
2023-11-16 18:03:36 +0800

    1

---
+++ Assets/DesignPatterns/Scripts/MainTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTest : MonoBehaviour
+{
+    public class A : IEnumerable
+    {
+        private List<int> _data;
+
+        public A(List<int> data)
+        {
+            _data = data;
+        }
+        
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AIterator : IEnumerator
+    {
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Current { get; }
+    }
+}
